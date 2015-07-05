e3fc4d58f823c65eaf49bccedc92927a78033970
liukaijv <liukaijv@foxmail.com>
2015-07-05 23:02:14 +0800

    Update ui.cityselect.js

--- js/core/ui.cityselect.js
+++ js/core/ui.cityselect.js
@@ -1,131 +1,130 @@
 // 省份联动
-;(function($){
-	$.fn.citySelect=function(settings){
-		if(this.length<1){return;};
-
-		// 默认值
-		settings=$.extend({
-			url:"js/city.js",
-			prov:null,
-			city:null,
-			dist:null,
-			nodata:null,
-			required:true
-		},settings);
-
-		var box_obj=this;
-		var prov_obj=box_obj.find(".prov");
-		var city_obj=box_obj.find(".city");
-		var dist_obj=box_obj.find(".dist");
-		var prov_val=settings.prov;
-		var city_val=settings.city;
-		var dist_val=settings.dist;
-		var select_prehtml=(settings.required) ? "" : "<option value=''>请选择</option>";
-		var city_json;
-
-		// 赋值市级函数
-		var cityStart=function(){
-			var prov_id=prov_obj.get(0).selectedIndex;
-			if(!settings.required){
-				prov_id--;
-			};
-			city_obj.empty().attr("disabled",true);
-			dist_obj.empty().attr("disabled",true);
-
-			if(prov_id<0||typeof(city_json.citylist[prov_id].c)=="undefined"){
-				if(settings.nodata=="none"){
-					city_obj.css("display","none");
-					dist_obj.css("display","none");
-				}else if(settings.nodata=="hidden"){
-					city_obj.css("visibility","hidden");
-					dist_obj.css("visibility","hidden");
-				};
-				return;
-			};
-			
-			// 遍历赋值市级下拉列表
-			temp_html=select_prehtml;
-			$.each(city_json.citylist[prov_id].c,function(i,city){
-				temp_html+="<option value='"+city.n+"'>"+city.n+"</option>";
-			});
-			city_obj.html(temp_html).attr("disabled",false).css({"display":"","visibility":""});
-			distStart();
+;(function($) {
+	$.fn.citySelect = function(settings) {
+		
+		if (this.length < 1) {
+			return
 		};
+		settings = $.extend({
+			url: "js/city.js",
+			prov: '130000',
+			city: null,
+			dist: null,
+			nodata: null,
+			required: true
+		}, settings);
+		var box_obj = this,
+		$this = $(this),
+		prov_obj = box_obj.find(".prov"),
+		city_obj = box_obj.find(".city"),
+		dist_obj = box_obj.find(".dist"),
+		prov_val = settings.prov,
+		city_val = settings.city,
+		dist_val = settings.dist,
+		select_prehtml = (settings.required) ? "" : "<option value=''>请选择</option>",		
+		city_json;
 
-		// 赋值地区（县）函数
-		var distStart=function(){
-			var prov_id=prov_obj.get(0).selectedIndex;
-			var city_id=city_obj.get(0).selectedIndex;
-			if(!settings.required){
-				prov_id--;
-				city_id--;
-			};
-			dist_obj.empty().attr("disabled",true);
-
-			if(prov_id<0||city_id<0||typeof(city_json.citylist[prov_id].c[city_id].a)=="undefined"){
-				if(settings.nodata=="none"){
-					dist_obj.css("display","none");
-				}else if(settings.nodata=="hidden"){
-					dist_obj.css("visibility","hidden");
+		var cityStart = function() {
+			var prov_id = prov_obj.get(0).selectedIndex;
+			if (!settings.required) {
+				prov_id--
+			};	
+			city_obj.empty().attr("disabled", true);
+			dist_obj.empty().attr("disabled", true);
+			if (prov_id < 0 || typeof(city_json[prov_id].items) == "undefined") {
+				if (settings.nodata == "none") {
+					city_obj.css("display", "none");
+					dist_obj.css("display", "none")
+				} else if (settings.nodata == "hidden") {
+					city_obj.css("visibility", "hidden");
+					dist_obj.css("visibility", "hidden")
 				};
-				return;
-			};
-			
-			// 遍历赋值市级下拉列表
-			temp_html=select_prehtml;
-			$.each(city_json.citylist[prov_id].c[city_id].a,function(i,dist){
-				temp_html+="<option value='"+dist.s+"'>"+dist.s+"</option>";
+				return
+			};				
+			temp_html = select_prehtml;
+			$.each(city_json[prov_id].items, function(i, city) {
+				temp_html += "<option value='" + city.Code + "'>" + city.Name + "</option>"
 			});
-			dist_obj.html(temp_html).attr("disabled",false).css({"display":"","visibility":""});
+			city_obj.html(temp_html).attr("disabled", false).css({
+				"display": "",
+				"visibility": ""
+			});
+			distStart()
 		};
-
-		var init=function(){
-			// 遍历赋值省份下拉列表
-			temp_html=select_prehtml;
-			$.each(city_json.citylist,function(i,prov){
-				temp_html+="<option value='"+prov.p+"'>"+prov.p+"</option>";
+		var distStart = function() {
+			var prov_id = prov_obj.get(0).selectedIndex;
+			var city_id = city_obj.get(0).selectedIndex;
+			if (!settings.required) {
+				prov_id--;
+				city_id--
+			};
+			dist_obj.empty().attr("disabled", true);
+			if (prov_id < 0 || city_id < 0 || typeof(city_json[prov_id].items[city_id].items) == "undefined") {
+				if (settings.nodata == "none") {
+					dist_obj.css("display", "none")
+				} else if (settings.nodata == "hidden") {
+					dist_obj.css("visibility", "hidden")
+				};
+				return
+			};
+			temp_html = select_prehtml;
+			$.each(city_json[prov_id].items[city_id].items, function(i, dist) {
+				temp_html += "<option value='" + dist.Code + "'>" + dist.Name + "</option>"
+			});
+			dist_obj.html(temp_html).attr("disabled", false).css({
+				"display": "",
+				"visibility": ""
+			})
+		};
+		var init = function() {
+			temp_html = select_prehtml;
+			$.each(city_json, function(i, prov) {
+				temp_html += "<option value='" + prov.Code + "'>" + prov.Name + "</option>"
 			});
 			prov_obj.html(temp_html);
-
-			// 若有传入省份与市级的值，则选中。（setTimeout为兼容IE6而设置）
-			setTimeout(function(){
-				if(settings.prov!=null){
+			setTimeout(function() {
+				if (settings.prov != null) {
 					prov_obj.val(settings.prov);
 					cityStart();
-					setTimeout(function(){
-						if(settings.city!=null){
+					setTimeout(function() {
+						if (settings.city != null) {
 							city_obj.val(settings.city);
 							distStart();
-							setTimeout(function(){
-								if(settings.dist!=null){
-									dist_obj.val(settings.dist);
-								};
-							},1);
-						};
-					},1);
-				};
-			},1);
-
-			// 选择省份时发生事件
-			prov_obj.bind("change",function(){
+							setTimeout(function() {
+								if (settings.dist != null) {
+									dist_obj.val(settings.dist)
+								}
+							}, 1)
+						}
+					}, 1)
+				}
+			}, 1);
+			prov_obj.bind("change", function() {				
 				cityStart();
+				$this.trigger('changed');
 			});
-
-			// 选择市级时发生事件
-			city_obj.bind("change",function(){
+			city_obj.bind("change", function() {
 				distStart();
+				$this.trigger('changed');
 			});
-		};
+			dist_obj.bind("change", function() {				
+				$this.trigger('changed');
+			});
 
-		// 设置省市json数据
-		if(typeof(settings.url)=="string"){
-			$.getJSON(settings.url,function(json){
-				city_json=json;
-				init();
-			});
-		}else{
-			city_json=settings.url;
-			init();
+			if(!settings.prov && !settings.city && !settings.dist){
+				prov_obj.trigger('change');
+			}
 		};
-	};
+		if (typeof(settings.url) == "string") {
+			$.getJSON(settings.url, function(json) {
+				city_json = json;
+				init()
+			})
+		} else {
+			city_json = settings.url;			
+			init()
+		}
+
+		return this;
+	}
 })(jQuery);
